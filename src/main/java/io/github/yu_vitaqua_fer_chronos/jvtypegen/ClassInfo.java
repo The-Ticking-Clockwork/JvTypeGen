78daa2e47dac030c3852b1fedf9595e50a7b3479
Yu-Vitaqua-fer-Chronos <random.access.exomemory@gmail.com>
2023-03-20 19:52:09 +0000

    Better serialisation, attempted to add some comments, added `type` field, innerclasses do not get serialised, unsure why. Interface and superclass support, doesn't repeat defined methods from superclass

--- src/main/java/io/github/yu_vitaqua_fer_chronos/jvtypegen/ClassInfo.java
+++ src/main/java/io/github/yu_vitaqua_fer_chronos/jvtypegen/ClassInfo.java
@@ -5,16 +5,47 @@ import java.util.List;
 
 public class ClassInfo {
     public String name;
+    public ClassType type;
+    public String superClass;
+    public List<String> implementationClasses;
     public String descriptorString;
+    public List<ConstructorInfo> constructors;
     public List<MethodInfo> methods;
     public List<FieldInfo> fields;
 
-    public ClassInfo(String name, String descriptorString, List<MethodInfo> methods,
+    public ClassInfo(String descriptorString, String name, ClassType type, String superClass,
+                     List<String> implementationClasses, List<ConstructorInfo> constructors, List<MethodInfo> methods,
                      List<FieldInfo> fields) {
-        this.name = name;
         this.descriptorString = descriptorString;
+        this.name = name;
+        this.type = type;
+        this.superClass = superClass;
+        this.implementationClasses = implementationClasses;
+        this.constructors = constructors;
         this.methods = methods;
         this.fields = fields;
+    }
+
+    public enum ClassType {
+        CLASS, ENUM, INTERFACE;
+
+        public static ClassType getValue(Class<?> cls) {
+            if (cls.isEnum()) {
+                return ENUM;
+            } else if (cls.isInterface()) {
+                return INTERFACE;
+            } else {
+                return CLASS;
+            }
+        }
+    }
+
+    public static class ConstructorInfo {
+        public HashMap<String, String> params;
+
+        public ConstructorInfo(HashMap<String, String> params) {
+            this.params = params;
+        }
     }
 
     public static class MethodInfo {
--- src/main/java/io/github/yu_vitaqua_fer_chronos/jvtypegen/Main.java
+++ src/main/java/io/github/yu_vitaqua_fer_chronos/jvtypegen/Main.java
@@ -2,46 +2,49 @@ package io.github.yu_vitaqua_fer_chronos.jvtypegen;
 
 import com.google.gson.Gson;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Parameter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.lang.reflect.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] descs) {
+        // Iterates through all arguments given, accepts class descriptors
         Gson gson = new Gson();
 
         for (String descriptor : descs) {
+            // Prints the serialised `ClassInfo` class returned from `analyseClass`
             System.out.println(gson.toJson(analyseClass(descriptor)));
         }
     }
 
     private static ClassInfo analyseClass(String descriptor) {
+        // Analyses a class to get info such as the methods, fields, constructors, etc within
         String clsDesc = descriptor;
         int arrayDepth = 0;
 
+        // Removes any `[` indicating an array
         while (clsDesc.startsWith("[")) {
             clsDesc = clsDesc.substring(1);
             arrayDepth += 1;
         }
 
+        // `L` means a JVM type, needs to be stripped for analysis
         if (clsDesc.startsWith("L")) {
             clsDesc = clsDesc.substring(1);
         }
 
+        // `;` means the end of a type descriptor, needs to be stripped for analysis
         if (descriptor.endsWith(";")) {
             clsDesc = clsDesc.substring(0, clsDesc.length()-1);
         }
 
+        // We get the current classloader so we can load the JVM type and analyse it
         ClassLoader clsLoader = Main.class.getClassLoader();
 
-        Class cls;
+        // Uninitialised variable
+        Class<?> cls;
 
+        // try-catch block to attempt to load the class, and to crash if it can't
         try {
             cls = clsLoader.loadClass(clsDesc.replace('/', '.'));
         } catch (ClassNotFoundException e) {
@@ -49,10 +52,58 @@ public class Main {
                     "` could not be found!", e);
         }
 
-        String clsName = cls.getCanonicalName();
+        // Get the class name (not package)
+        String clsName = cls.getName();
 
+        // Interfaces the class implements
+        List<String> implClses = new ArrayList<>();
+
+        // Get all interfaces the class uses
+        for (Class<?> implCls : cls.getInterfaces()) {
+            implClses.add(implCls.getName());
+        }
+
+        // Classes such as `Object` (likely the only class?) don't have a superclass
+        String superCls = "";
+
+        // Get the superclass if it exists
+        if(cls.getSuperclass() != null) {
+            superCls = cls.getSuperclass().getName();
+        }
+
+        ClassInfo.ClassType clsType = ClassInfo.ClassType.getValue(cls);
+
+        // Initialise the lists for constructors, methods and fields
+        List<ClassInfo.ConstructorInfo> constructors = new ArrayList<>();
         ArrayList<ClassInfo.MethodInfo> methods = new ArrayList<>();
         ArrayList<ClassInfo.FieldInfo> fields = new ArrayList<>();
+
+        // Iterate through all constructors in the class
+        for (Constructor<?> c : cls.getConstructors()) {
+            LinkedHashMap<String, String> params = new LinkedHashMap<>();
+
+            // Used to see if the constructor exists in the parent class (if there is a parent)
+            ArrayList<Class<?>> paramTypes = new ArrayList<>();
+
+            for (Parameter p : c.getParameters()) {
+                params.put(p.getName(), p.getType().getName());
+
+                if (!superCls.equals("")) {
+                    paramTypes.add(p.getType());
+                }
+            }
+
+            // Checks if the superclass exists, then checks if the constructor exists for the class
+            if (!superCls.equals("")) {
+                try {
+                    cls.getSuperclass().getDeclaredConstructor(paramTypes.toArray(new Class[]{}));
+                } catch (NoSuchMethodException e) {
+                    constructors.add(new ClassInfo.ConstructorInfo(params));
+                }
+            } else {
+                constructors.add(new ClassInfo.ConstructorInfo(params));
+            }
+        }
 
         for (Method m : cls.getMethods()) {
             String methodName = m.getName();
@@ -62,19 +113,53 @@ public class Main {
 
             LinkedHashMap<String, String> params = new LinkedHashMap<>();
 
+            // Used to see if the constructor exists in the parent class (if there is a parent)
+            ArrayList<Class<?>> paramTypes = new ArrayList<>();
+
             for (Parameter p : m.getParameters()) {
-                params.put(p.getName(), p.getType().getSimpleName());
+                params.put(p.getName(), p.getType().getName());
+
+                if (!superCls.equals("")) {
+                    paramTypes.add(p.getType());
+                }
             }
 
-
-            methods.add(new ClassInfo.MethodInfo(methodName, params, returnType, isStatic));
+            // Checks if the superclass exists, then checks if the method exists for the class
+            if (!superCls.equals("")) {
+                try {
+                    cls.getSuperclass().getDeclaredMethod(methodName, paramTypes.toArray(new Class[]{}));
+                } catch (NoSuchMethodException e) {
+                    methods.add(new ClassInfo.MethodInfo(methodName, params, returnType, isStatic));
+                }
+            } else {
+                methods.add(new ClassInfo.MethodInfo(methodName, params, returnType, isStatic));
+            }
         }
 
         for (Field f : cls.getFields()) {
-            fields.add(new ClassInfo.FieldInfo(f.getName(), f.getType().getSimpleName(),
-                    Modifier.isStatic(f.getModifiers())));
+            // Check if the superclass exists, and if it does, check if the field exists in the superclass
+            if (!superCls.equals("")) {
+                try {
+                    cls.getSuperclass().getDeclaredField(f.getName());
+                } catch (NoSuchFieldException e) {
+                    fields.add(new ClassInfo.FieldInfo(f.getName(), f.getType().getName(),
+                            Modifier.isStatic(f.getModifiers())));
+                }
+            } else {
+                fields.add(new ClassInfo.FieldInfo(f.getName(), f.getType().getName(),
+                        Modifier.isStatic(f.getModifiers())));
+            }
         }
 
-        return new ClassInfo(clsName, descriptor, methods, fields);
+        return new ClassInfo(
+                cls.descriptorString(),
+                clsName,
+                clsType,
+                superCls,
+                implClses,
+                constructors,
+                methods,
+                fields
+        );
     }
 }
